d21c124bb6768e1134ddcaf191e6eba7f818ad51
Sagar Chamling <sgr.raee@gmail.com>
2023-09-30 17:53:30 -0400

    Work on design

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import './App.css';
+
+import './css/cvss.css';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main />
   );
 }
 
